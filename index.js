9615bd793f454229b86c20bab7798ccbad325995
vankatwijk <hendrikus.hpvk@gmail.com>
2021-06-15 13:45:53 -0800

    init project packages

---
+++ index.js
@@ -0,0 +1,8 @@
+const puppeteer = require('puppeteer');
+const $ = require('cheerio');
+const CronJob = require('cron').CronJob;
+const nodemailer = require('nodemailer');
+
+const url = 'https://www.google.com/search?q=b188bet';
+
+const browser = await
