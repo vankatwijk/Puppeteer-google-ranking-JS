134d40673fd44eed7783aa2799242b1587241edc
vankatwijk <hendrikus.hpvk@gmail.com>
2021-06-16 13:45:53 -0800

    counfigure browser function

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ const $ = require('cheerio');
 const CronJob = require('cron').CronJob;
 const nodemailer = require('nodemailer');
 
-const url = 'https://www.google.com/search?q=b188bet';
+const url = 'https://www.amazon.de/-/en/Nintendo-Switch-Console-gray/dp/B07W13KJZC/ref=sr_1_2?crid=34U1V2D2DAQ7I&dchild=1&keywords=nintendo+switch&qid=1623762299&sprefix=nintendo%2Caps%2C210&sr=8-2';
 
-const browser = await
+
+async function configureBrowser() {
+
+    const browser = await puppeteer.launch()
+    const page = await puppeteer.newPage();
+    await page.goto(url);
+    return page;
+}
+
+async function checkPrice() {
+
+
+}
