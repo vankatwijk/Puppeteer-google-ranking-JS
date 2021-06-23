28a5a2c9d1acc788d29ccad6c6b3788a4f18668d
vankatwijk <hendrikus.hpvk@gmail.com>
2021-06-23 13:35:53 -0800

    amazon page load

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const nodemailer = require('nodemailer');
 const url = 'https://www.amazon.de/-/en/Nintendo-Switch-Console-gray/dp/B07W13KJZC/ref=sr_1_2?crid=34U1V2D2DAQ7I&dchild=1&keywords=nintendo+switch&qid=1623762299&sprefix=nintendo%2Caps%2C210&sr=8-2';
 
 
+
+function delay(time) {
+    return new Promise(function(resolve) {
+        setTimeout(resolve, time)
+    });
+}
+
 async function configureBrowser() {
 
     const browser = await puppeteer.launch()
